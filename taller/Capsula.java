9cce95bafcb8bf2c0191d5a4a2d61dfd772bd06f
GYEISSON <yeison7f@gmail.com>
2018-10-05 13:06:03 -0500

    viernes avance

---
+++ taller/Capsula.java
@@ -0,0 +1,22 @@
+import java.util.*;
+/**
+ * Write a description of class Capsula here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Capsula extends Maquina
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<Capsula> otrasCapsulas;
+
+    /**
+     * Constructor for objects of class Capsula
+     */
+    public Capsula()
+    {
+        otrasCapsulas = new ArrayList<Capsula>();        
+    }
+    
+    
+}
---
+++ taller/Nodriza.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+/**
+ * Write a description of class Nodrisa here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Nodriza extends Maquina
+
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<Capsula> capsulas;
+
+    /**
+     * Constructor for objects of class Nodrisa
+     */
+    public Nodriza()
+    {
+        capsulas = new ArrayList<Capsula>();
+    }
+    
+}
